caee39caf07ae8d6616ec972f7d418ea2da659ff
SusuBea <suhajdabea@gmail.com>
2022-01-29 01:47:47 +0100

    correct typo

---
+++ index.js
@@ -0,0 +1,11 @@
+const light = document.getElementById(on);
+const dark = document.getElementById(off);
+
+on.addEventListener("submit", function (event) {
+    
+}
+
+)
+
+orm.addEventListener("submit", function (event) {
+    event.preventDefault();
